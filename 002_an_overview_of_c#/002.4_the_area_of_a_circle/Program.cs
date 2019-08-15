54c6248d690fb482f97cb2f1d036ffa472c024f6
Віктор Шереш <sheregi1000@gmail.com>
2019-08-15 19:24:18 +0300

    A circle

---
+++ 002_an_overview_of_c#/002.4_the_area_of_a_circle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            double area;
+
+            radius = 10.0;
+            area = radius * radius * 3.1416;
+
+            Console.WriteLine("Are is " + area);
+
+        }
+    }
+}
