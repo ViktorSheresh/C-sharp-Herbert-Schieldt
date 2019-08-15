09ea323e773d67db1dda3b5dec09f6385ba8b90f
Віктор Шереш <sheregi1000@gmail.com>
2019-08-15 19:39:53 +0300

    A block of code

---
+++ 002_an_overview_of_c#/002.7_a_block_of_code/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_code_block
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Demonstrate a block of code
+
+            int i, j, d;
+
+            i = 5;
+            j = 10;
+
+            //The target of this if is a block.
+            if (i != 0)
+            {
+                Console.WriteLine("i does not equal zero");
+            }
+            d = j / i;
+            Console.WriteLine("j / i is" + d);
+        }
+    }
+}
