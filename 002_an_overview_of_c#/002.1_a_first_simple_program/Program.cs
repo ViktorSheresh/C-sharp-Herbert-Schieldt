635005fe8f8f480f194b5e41fb9cc10907d8819c
Віктор Шереш <sheregi1000@gmail.com>
2019-08-15 19:11:26 +0300

    A simple program

---
+++ 002_an_overview_of_c#/002.1_a_first_simple_program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002._1_a_first_simple_program
+{
+    class Program
+    {
+        // A C# program begins with a call to Main().
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A simple program on C#");
+        }
+    }
+}
