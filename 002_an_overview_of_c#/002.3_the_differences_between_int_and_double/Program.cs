684362db76f732323ef94fea8c1d6da1d96858fb
Віктор Шереш <sheregi1000@gmail.com>
2019-08-15 19:20:41 +0300

    This program illustrates the differences between int and double

---
+++ 002_an_overview_of_c#/002.3_the_differences_between_int_and_double/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Int_and_double
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ivar;       // this declares an int variable
+            double dvar;    // this declares a floating-point variable
+
+            ivar = 100;     // assign ivar the value 100
+            dvar = 100.0;   // assign dvar the value 100.0
+
+            Console.WriteLine("Original value of ivar: " + ivar);
+            Console.WriteLine("Original value of dvar: " + dvar);
+
+            Console.WriteLine(); // print a blank line
+
+            //Now, divide both by 3.
+            ivar = ivar / 3;
+            dvar = dvar / 3.0;
+
+            Console.WriteLine("ivar after division: " + ivar);
+            Console.WriteLine("ivar afte division: " + dvar);
+        }
+    }
+}
