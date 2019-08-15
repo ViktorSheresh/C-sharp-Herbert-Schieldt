4f28a9d6e9596ca4d02d2d67cc442f1d8c1982c9
Віктор Шереш <sheregi1000@gmail.com>
2019-08-15 19:36:12 +0300

    The for loop

---
+++ 002_an_overview_of_c#/002.6_the_for_loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Demonstrate the for loop
+
+            int count;
+
+            for (count = 0; count < 5; count = count + 1)
+                Console.WriteLine("This is count: " + count);
+            Console.WriteLine("Done!");
+        }
+    }
+}
