5aee30dd77fd3ed1ca1585493e42dc92111ac597
Віктор Шереш <sheregi1000@gmail.com>
2019-08-15 19:12:03 +0300

    This program demonstrate variables

---
+++ 002_an_overview_of_c#/002.2_a_second_simple_program/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_second_simple_program
+{
+    class Program
+    {
+        // This program demonstrates variables.
+        static void Main(string[] args)
+        {
+            int x; // this declares a variable
+            int y; // this declares another variable
+
+            x = 100; //this assigns 100 to x
+
+            Console.WriteLine("x contains " + x);
+
+            y = x / 2;
+            Console.Write("y containts x / 2: ");
+            Console.WriteLine(y);
+        }
+    }
+}
