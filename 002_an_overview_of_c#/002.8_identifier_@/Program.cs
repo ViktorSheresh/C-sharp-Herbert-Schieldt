b4bd0ae90a257e78e26f4c74dae6f1f298f22fd2
Віктор Шереш <sheregi1000@gmail.com>
2019-08-16 16:02:31 +0300

    The identifier

---
+++ 002_an_overview_of_c#/002.8_identifier_@/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The___identifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Demonstrate an @ identifier
+
+            int @if; // use if as an identifier
+            for (@if = 0; @if < 10; @if++)
+                Console.WriteLine("@if is " + @if);
+        }
+    }
+}
