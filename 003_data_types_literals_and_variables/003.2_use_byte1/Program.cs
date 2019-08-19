79070c80d9076fca32637a2ac9e3ae562b59f6a8
Віктор Шереш <sheregi1000@gmail.com>
2019-08-19 22:22:07 +0300

    Byte

---
+++ 003_data_types_literals_and_variables/003.2_use_byte1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Byte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Use byte
+            byte x;
+            int sum;
+
+            sum = 0;
+            for (x = 1; x <= 0; x++)
+                sum = sum + x;
+            Console.WriteLine("Summation of 100 is " + sum);
+        }
+    }
+}
