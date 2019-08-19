9cb8769ea0b0ae27e3ffa28d47eb94f7e779ed0d
Віктор Шереш <sheregi1000@gmail.com>
2019-08-19 22:10:00 +0300

    Distance from the Earth to the sun

---
+++ 003_data_types_literals_and_variables/003.1_the_distance_from_to_the_Earth_to_the_Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_distance_from_Earth_to_the_Sun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Compute the distance from the Earth to the Sun, in inches.
+
+            long inches;
+            long miles;
+
+            miles = 93000000; // 93,000,000 miles to the Sun
+            // 5,280 feet in a mile, 12 inches in a foot.
+            inches = miles * 5280 * 12;
+
+            Console.WriteLine("Distance to the sun: " + inches + " inches.");
+        }
+    }
+}
