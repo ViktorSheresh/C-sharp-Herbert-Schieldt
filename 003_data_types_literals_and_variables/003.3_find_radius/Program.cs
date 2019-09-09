ac01cfa9565d9987e3df6f556f83ee95dcfa9e2f
Віктор Шереш <sheregi1000@gmail.com>
2019-09-10 00:06:33 +0300

    Find radius

---
+++ 003_data_types_literals_and_variables/003.3_find_radius/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003._3_find_radius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double r;
+            Double area;
+
+            area = 10.0;
+
+            r = Math.Sqrt(area / 3.1416);
+            Console.WriteLine("Radius is " + r);
+        }
+    }
+}
