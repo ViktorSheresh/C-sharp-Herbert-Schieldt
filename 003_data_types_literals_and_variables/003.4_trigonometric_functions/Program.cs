78e88298b7a0eaa6d97021b10735e4eac0e7ff39
Віктор Шереш <sheregi1000@gmail.com>
2019-09-10 00:15:56 +0300

    Trigonometric functions

---
+++ 003_data_types_literals_and_variables/003.4_trigonometric_functions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003._4_trigonometric_functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double theta; // angle in radians
+
+            for (theta = 0.1; theta <= 1.0; theta += 0.1)
+            {
+                Console.WriteLine("Sine of " + theta + " is " + Math.Sin(theta));
+                Console.WriteLine("Cosine of " + theta + " is " + Math.Cos(theta));
+                Console.WriteLine("Tangent of " + theta + " is " + Math.Tan(theta));
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
