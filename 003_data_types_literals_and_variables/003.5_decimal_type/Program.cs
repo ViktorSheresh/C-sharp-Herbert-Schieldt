32e55a46996acfe5201c3a7f57e53ec58212d78e
Віктор Шереш <sheregi1000@gmail.com>
2019-09-10 00:26:25 +0300

    decimal type

---
+++ 003_data_types_literals_and_variables/003.5_decimal_type/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003._5_decimal_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal price;
+            decimal discount;
+            decimal discounted_price;
+            // Compute discounted price.
+            price = 19.95m;
+            discount = 0.15m; // discount rate is 15%
+            discounted_price = price - (price * discount);
+            Console.WriteLine("Discounted price: $" + discounted_price);
+        }
+    }
+}
