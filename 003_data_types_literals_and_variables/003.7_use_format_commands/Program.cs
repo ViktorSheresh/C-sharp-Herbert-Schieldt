f11b7ca9b3316d682e08469dfc991a265e200f62
Віктор Шереш <sheregi1000@gmail.com>
2019-09-17 19:33:11 +0300

    Format commands

---
+++ 003_data_types_literals_and_variables/003.7_use_format_commands/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003._7_use_format_commands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Value\tSquared\tCubed");
+            for (i = 1; i < 10; i++)
+                Console.WriteLine("{0}\t{1}\t{2}", i, i * i, i * i * i);
+        }
+    }
+}
