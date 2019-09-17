3aff3beee3bee57ab3142867f787d33f2b8556bb
Віктор Шереш <sheregi1000@gmail.com>
2019-09-17 19:33:38 +0300

    Monetary value

---
+++ 003_data_types_literals_and_variables/003.8_the_c_format_(monetary_values)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003._8_the_c_format__monetary_values_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal price;
+            decimal discount;
+            decimal discounted_price;
+            // Compute discounted price.
+            price = 19.95m;
+            discount = 0.15m; // discount rate is 15%
+            discounted_price = price - (price * discount);
+            Console.WriteLine("Discounted price: {0:C}", discounted_price);
+        }
+    }
+}
